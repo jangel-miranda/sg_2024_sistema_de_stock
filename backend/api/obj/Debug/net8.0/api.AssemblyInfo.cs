59bc34669e1a8a0f9ff1eeda676fc3a2163c6eba
gissgroa <57072138+gdroa-edu@users.noreply.github.com>
2024-05-03 13:28:05 -0400

    ???

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb44e3ae093615dcd68761b9649f368a1f07297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5351da351486f4d33ac35aa5d2ea7453776d2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
